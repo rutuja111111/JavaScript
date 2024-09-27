4667b5389f9b0913a3535b74610bc29f25e52200
Hrutuja Aher <139643261+rutuja111111@users.noreply.github.com>
2024-09-27 15:25:17 +0530

    Update GRC.js

--- Generate_Random_Color/GRC.js
+++ Generate_Random_Color/GRC.js
@@ -1,1 +1,23 @@
 
+
+
+let btn=document.querySelector("button");
+
+btn.addEventListener("click",function(){
+    let h3=document.querySelector("h3");
+    let randomColor=getRandomColors();
+    let div=document.querySelector("div");
+    div.style.backgroundColor=randomColor;
+    h3.innerText=randomColor;
+    console.log("color updated!");
+});
+function getRandomColors()
+{
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+    
+    let color=`rgb(${red},${green},${blue})`;
+    return color;
+}
+
