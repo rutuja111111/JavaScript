ba904f1d72c7cd5679812c3067dacae0c3ebe50a
Hrutuja Aher <139643261+rutuja111111@users.noreply.github.com>
2024-09-27 15:49:59 +0530

    Update app.js

--- Todo list/app.js
+++ Todo list/app.js
@@ -1,1 +1,27 @@
+let input=document.querySelector("input");
+let btn=document.querySelector("button");
+let ul=document.querySelector("ul");
 
+btn.addEventListener("click",function(){
+    let li=document.createElement("li");
+    li.innerText=input.value;
+
+    let delbtn=document.createElement("button");
+    delbtn.innerText="delete";
+    delbtn.classList.add("delete");
+    li.appendChild(delbtn);
+
+    ul.appendChild(li);
+    
+ 
+   input.value="";
+   
+});
+ul.addEventListener("click",function(event){
+    if(event.target.nodeName=="BUTTON")
+    {
+        let l=event.target.parentElement;
+        l.remove();
+        console.log("deleted");
+    }
+});
